6930715132743ac66538b6dbe88bf2a7430a9b82
sararbc <68914827+sararbc@users.noreply.github.com>
2020-09-16 22:25:06 +0100

    Search Engine working

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
-import DisplayDate from "./DisplayDate";
+import WeatherInfo from "./WeatherInfo";
+
 import axios from "axios";
 
 export default function SearchEngine(props) {
   let [weatherData, setWeatherData] = useState({ loaded: false });
-  //let [city, setCity] = useState(props.defaultCity);
+  let [city, setCity] = useState(props.defaultCity);
 
-  function handleSubmit() {}
-  //function updateCity(event) {
-  //event.preventDefault();
-  //setCity(event.target.value);
-  //}
+  function search() {
+    const apiKey = "6c3b1017063f8a3764595f3d1af14037";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeather);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
 
   function displayWeather(response) {
     setWeatherData({
       loaded: true,
+      city: response.data.name,
       date: new Date(response.data.dt * 1000),
       temperature: Math.round(response.data.main.temp),
       maxTempertaure: Math.round(response.data.main.temp_max),
@@ -35,7 +47,7 @@ export default function SearchEngine(props) {
               type="text"
               className="form-control"
               placeholder="Search city"
-              //onChange={updateCity}
+              onChange={updateCity}
             />
             <span>
               <input type="submit" value="Search" className="btn btn-primary" />
@@ -48,73 +60,11 @@ export default function SearchEngine(props) {
             </button>
           </div>
         </form>
-        <div className="DisplayTime">
-          <h1>{props.defaultCity}</h1>
-          <h2>
-            <DisplayDate date={weatherData.date} />
-          </h2>
-        </div>
-        <div className="ShowTemperature">
-          <div className="row">
-            <div className="col-6">
-              <div className="weather">
-                <p>
-                  <img
-                    src="https://openweathermap.org/img/wn/10d@2x.png"
-                    id="icon"
-                    alt={weatherData.description}
-                  />
-                  <span id="weather-description">
-                    {weatherData.description}
-                  </span>
-                </p>
-              </div>
-              <div className="temperature">
-                <p>
-                  <span id="show-temperature">
-                    {Math.round(weatherData.temperature)}
-                  </span>
-                  <span id="celsius-temperature" className="active">
-                    °C
-                  </span>
-                  <span className="convert-temperature"> |</span>
-                  <span id="fahrenheit-temperature">°F</span>
-                </p>
-              </div>
-            </div>
-            <div className="col-3  min-max-temperature">
-              <p>
-                <span id="max-temperature">{weatherData.maxTempertaure}º</span>
-                <hr />
-                <span className="min-temperature" id="min-temperature">
-                  {weatherData.minTempertaure}º
-                </span>
-              </p>
-            </div>
-            <div className="col-3 ">
-              <ul>
-                <li>
-                  <i className="fas fa-tint"></i>
-                  <span className="description">Humidity:</span>
-                  <span id="show-humidity"> {weatherData.humidity} %</span>
-                </li>
-                <li>
-                  <i className="fas fa-wind"></i>
-                  <span className="description wind">Wind:</span>
-                  <span id="show-wind"> {weatherData.wind}km/h</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "6c3b1017063f8a3764595f3d1af14037";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(displayWeather);
-
+    search();
     return "Loading...";
   }
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,66 @@
+import React from "react";
+import DisplayDate from "./DisplayDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div className="DisplayTime">
+        <h1>{props.data.city}</h1>
+        <h2>
+          <DisplayDate date={props.data.date} />
+        </h2>
+      </div>
+      <div className="ShowTemperature">
+        <div className="row">
+          <div className="col-6">
+            <div className="weather">
+              <p>
+                <img
+                  src="https://openweathermap.org/img/wn/10d@2x.png"
+                  id="icon"
+                  alt={props.data.description}
+                />
+                <span id="weather-description">{props.data.description}</span>
+              </p>
+            </div>
+            <div className="temperature">
+              <p>
+                <span id="show-temperature">
+                  {Math.round(props.data.temperature)}
+                </span>
+                <span id="celsius-temperature" className="active">
+                  °C
+                </span>
+                <span className="convert-temperature"> |</span>
+                <span id="fahrenheit-temperature">°F</span>
+              </p>
+            </div>
+          </div>
+          <div className="col-3  min-max-temperature">
+            <p>
+              <span id="max-temperature">{props.data.maxTempertaure}º</span>
+              <hr />
+              <span className="min-temperature" id="min-temperature">
+                {props.data.minTempertaure}º
+              </span>
+            </p>
+          </div>
+          <div className="col-3 ">
+            <ul>
+              <li>
+                <i className="fas fa-tint"></i>
+                <span className="description">Humidity:</span>
+                <span id="show-humidity"> {props.data.humidity} %</span>
+              </li>
+              <li>
+                <i className="fas fa-wind"></i>
+                <span className="description wind">Wind:</span>
+                <span id="show-wind"> {props.data.wind}km/h</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
